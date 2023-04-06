b2f02a082150006620dfb164f996521082f7e6fe
Alejandra Duraaaaan <duran.aleja@gmail.com>
2023-04-06 22:14:06 +0200

    pagina de contacto terminada

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,57 +1,47 @@
-import React from 'react'
-import Card from '../Components/Card'
-import { useState } from "react"
-
+import React from "react";
+import { useState } from "react";
 
 const Contact = () => {
   const [user, setUser] = useState({
     name: "",
-    email: "",
+    email: undefined,
   });
 
-    const [show, setShow] = useState(false);
-    const [err, setErr] = useState(false);
+  const [show, setShow] = useState(false);
+  const [err, setErr] = useState(false);
 
-    const alertar = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (user.name.length >= 5 && user.email) {
+      setShow(true);
+      setErr(false);
       alert(`Gracias ${user.name}, te contactaremos cuando antes vía mail!`);
-    };
-    
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      if (user.nombre.length >= 5) {
-        setShow(true);
-        setErr(false);
-      } else {
-        setShow(false);
-        setErr(true);
-      }
-
-    };
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Nombre Completo </label>
-          <input
-            type="text"
-            value={user.nombre}
-            onChange={(e) => setUser({ ...user, nombre: e.target.value })}
-          />
-          <label>Email</label>
-          <input
-            type="text"
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-          />
-        <button onClick={alertar}>Enviar</button>
-
-        
-
-        </form>
-        {err ? "Por favor verifique su información nuevamente" : null}
-        {show && <Card nombre={user.nombre} email={user.email} />}
-
-      </div>
-    );
+    } else {
+      setShow(false);
+      setErr(true);
+      alert('Por favor verifique su información nuevamente');
+    }
   };
 
-export default Contact
+  return (
+    <div className="content-top-margin">
+       <h2>Want to know more?</h2>
+      <p>Send us your questions and we will contact you</p>
+      <form onSubmit={handleSubmit}>
+        <label className="form-spacer" >Full Name</label>
+        <input className="form-spacer" id="user.name"
+          type="text"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+        <label className="form-spacer">Email</label>
+        <input className="form-spacer"
+          type="email"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input type="submit" className="form-spacer" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
